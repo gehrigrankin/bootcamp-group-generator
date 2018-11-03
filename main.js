4a75f0ad13ac54ab7150c71726f42e95f1458305
gehrig rankin <rgrankin22@gmail.com>
2018-11-02 23:58:33 -0700

    started group check

--- main.js
+++ main.js
@@ -15,7 +15,9 @@ const group1 = new Group()
 
 let groups = []
 
-makeGroups(7)
+classAvg();
+makeGroups(7);
+groupCheck();
 
 function makeGroups(numGroups) {
     for (let i = 0; i < numGroups; i++) {
@@ -45,7 +47,7 @@ function pickStudent(){
         let student = students[Math.floor(Math.random() * students.length)];
         console.log(student.name)
 
-        groupStudents.push(student.name)
+        groupStudents.push(student)
     
         let index = students.indexOf(student);
         students.splice(index, 1);
@@ -54,6 +56,60 @@ function pickStudent(){
     return groupStudents;
 }
 
+function groupCheck(){
+    let flag = false;
+
+    groups.sort(function(a, b){
+        return b.skill-a.skill
+    })
+
+    while (!flag){
+        let groupAvgs = [];
+
+        for (let i = 0; i < groups.length; i++){
+            let groupTotal = 0;
+
+            for (let j = 0; j < groups[i].students.length; j++){
+                groupTotal += groups[i].students[j].skill
+            }
+
+            let avg = groupTotal / groups[i].students.length
+            console.log(avg)
+            groupAvgs.push(avg)
+            
+            console.log(groupAvgs.sort())
+            
+            if (groupAvgs[0] + .9 < groupAvgs[groupAvgs.length]){
+                let weakGroupSkills = []
+                let strongGroupSkills = []
+                
+                for (let j = 0; j < groups[i].students.length; j++){
+                    weakGroupSkills.push(groups[i].students[j].skill)
+                }
+
+                let weakLink = groups[i].student[weekGroupSkills.indexOf(weekGroupSkills.min())];
+                let strongLink = groups[].student[weekGroupSkills.indexOf(weekGroupSkills.min())];
+            }
+        }
+
+        
+
+        flag = true
+    }
+}
+
+function classAvg(){
+    let total = 0;
+
+    for (let i = 0; i < students.length; i++){
+        total += students[i].skill
+    }
+
+    let avg = total / students.length;
+
+    console.log(avg)
+    return avg
+}
 
 
 // students.shuffle()
